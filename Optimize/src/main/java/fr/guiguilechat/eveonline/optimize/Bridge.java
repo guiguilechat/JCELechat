836e87b56c7e90dfb6b45a0905f055ab17b556e7
guiguilechat <lechatguigui@gmail.com>
2017-03-15 17:01:14 +0100

    added low/medium/high slots management in bridge.

--- Optimize/src/main/java/fr/guiguilechat/eveonline/optimize/Bridge.java
+++ Optimize/src/main/java/fr/guiguilechat/eveonline/optimize/Bridge.java
@@ -2,6 +2,7 @@ package fr.guiguilechat.eveonline.optimize;
 
 import fr.guiguilechat.eveonline.database.Database;
 import fr.guiguilechat.eveonline.database.elements.Hull;
+import fr.guiguilechat.eveonline.database.elements.Module;
 
 public class Bridge {
 
@@ -18,6 +19,10 @@ public class Bridge {
 		hullsIds = database.hulls.keySet().stream().mapToInt(i -> i).toArray();
 	}
 
+	public Hull[] getHulls() {
+		return hulls;
+	}
+
 	public int getEveHullId(int internalHudId) {
 		return hullsIds[internalHudId];
 	}
@@ -29,6 +34,36 @@ public class Bridge {
 			hullSpeed = database.hulls.values().stream().mapToInt(h -> h.attributes.velocity).toArray();
 		}
 		return hullSpeed;
+	}
+
+	protected Module[] highModules;
+
+	public Module[] getHighModules() {
+		if (highModules == null) {
+			highModules = database.modules.values().stream().filter(m -> m.attributes.slot.equals("high"))
+					.toArray(Module[]::new);
+		}
+		return highModules;
+	}
+
+	protected Module[] mediumModules;
+
+	public Module[] getMediumModules() {
+		if (mediumModules == null) {
+			mediumModules = database.modules.values().stream().filter(m -> m.attributes.slot.equals("medium"))
+					.toArray(Module[]::new);
+		}
+		return highModules;
+	}
+
+	protected Module[] lowModules;
+
+	public Module[] getLowModules() {
+		if (lowModules == null) {
+			lowModules = database.modules.values().stream().filter(m -> m.attributes.slot.equals("low"))
+					.toArray(Module[]::new);
+		}
+		return highModules;
 	}
 
 
