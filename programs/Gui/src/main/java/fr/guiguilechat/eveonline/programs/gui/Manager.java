b38b5bb6934579e7c0846a21f3f0b88a8ca8a099
guiguilechat <lechatguigui@gmail.com>
2017-08-22 21:47:30 +0200

    updates.

---
+++ programs/Gui/src/main/java/fr/guiguilechat/eveonline/programs/gui/Manager.java
@@ -0,0 +1,20 @@
+package fr.guiguilechat.eveonline.programs.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+@SuppressWarnings("restriction")
+public class Manager extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("eve toon manager");
+		// TODO Auto-generated method stub
+
+	}
+
+}
