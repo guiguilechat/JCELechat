01af223a1f2188769ea3e666a6d019e3d202498d
guiguilechat <lechatguigui@gmail.com>
2018-01-07 22:12:50 +0100

    adding character infos, remove _ok in response structures.

--- model/ESI/src/main/java/fr/guiguilechat/eveonline/model/esi/connect/modeled/Character.java
+++ model/ESI/src/main/java/fr/guiguilechat/eveonline/model/esi/connect/modeled/Character.java
@@ -1,6 +1,7 @@
 package fr.guiguilechat.eveonline.model.esi.connect.modeled;
 
 import fr.guiguilechat.eveonline.model.esi.connect.ESIRawConnection;
+import is.ccp.tech.esi.responses.R_get_characters_character_id_ok;
 
 public class Character {
 
@@ -8,6 +9,15 @@ public class Character {
 
 	public Character(ESIRawConnection raw) {
 		this.raw = raw;
+	}
+
+	R_get_characters_character_id_ok infos = null;
+
+	public R_get_characters_character_id_ok getInfos() {
+		if (infos == null) {
+			infos = raw.get_characters_character_id(raw.verify().CharacterID);
+		}
+		return infos;
 	}
 
 }
