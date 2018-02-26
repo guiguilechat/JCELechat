388223c1d1c21f31774c37884387e242e0a0ed28
guiguilechat <lechatguigui@gmail.com>
2018-02-26 23:25:27 +0100

    started route, made market split buy and sell orders

--- model/ESI/src/main/java/fr/guiguilechat/eveonline/model/esi/modeled/Character.java
+++ model/ESI/src/main/java/fr/guiguilechat/eveonline/model/esi/modeled/Character.java
@@ -4,14 +4,18 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import fr.guiguilechat.eveonline.model.esi.ESIAccount;
 import is.ccp.tech.esi.responses.R_get_characters_character_id;
+import is.ccp.tech.esi.responses.R_get_characters_character_id_bookmarks;
+import is.ccp.tech.esi.responses.R_get_characters_character_id_bookmarks_folders;
 import is.ccp.tech.esi.responses.R_get_characters_character_id_industry_jobs;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 
 public class Character {
 
@@ -78,37 +82,95 @@ public class Character {
 
 	private long jobsCacheEnd = 0;
 
-	ObservableList<R_get_characters_character_id_industry_jobs> jobsCache = FXCollections.observableArrayList();
+	private ObservableList<R_get_characters_character_id_industry_jobs> jobsCache = FXCollections.observableArrayList();
 
 	/**
 	 * fetch the list of industry jobs for this character. If the cache delay is
 	 * not expired, uses the cached version.
-	 * 
+	 *
 	 * @return the internal cache of the jobs for this character. successive calls
 	 *         will return the same value.
 	 *
 	 */
 	public ObservableList<R_get_characters_character_id_industry_jobs> getIndustryJobs() {
-		if (System.currentTimeMillis() >= jobsCacheEnd) {
-			ArrayList<R_get_characters_character_id_industry_jobs> ret = new ArrayList<>();
-			int maxPage = 1;
-			jobsCacheEnd = Long.MAX_VALUE;
-			for (int page = 1; page <= maxPage; page++) {
-				Map<String, List<String>> headers = new HashMap<>();
-				ret.addAll(Arrays.asList(con.raw.get_characters_character_id_industry_jobs(con.characterId(), false, headers)));
-				if (page == 1) {
-					String pages = headers.containsKey("x-pages") ? headers.get("x-pages").get(0) : null;
-					maxPage = pages == null ? 1 : Integer.parseInt(pages);
+		synchronized (jobsCache) {
+			if (System.currentTimeMillis() >= jobsCacheEnd) {
+				ArrayList<R_get_characters_character_id_industry_jobs> ret = new ArrayList<>();
+				int maxPage = 1;
+				jobsCacheEnd = Long.MAX_VALUE;
+				for (int page = 1; page <= maxPage; page++) {
+					Map<String, List<String>> headers = new HashMap<>();
+					ret.addAll(
+							Arrays.asList(con.raw.get_characters_character_id_industry_jobs(con.characterId(), false, headers)));
+					if (page == 1) {
+						String pages = headers.containsKey("x-pages") ? headers.get("x-pages").get(0) : null;
+						maxPage = pages == null ? 1 : Integer.parseInt(pages);
+						jobsCacheEnd = System.currentTimeMillis()
+								+ 1000 * ZonedDateTime.parse(headers.get("Expires").get(0), ESIAccount.formatter).toEpochSecond()
+								- 1000 * ZonedDateTime.parse(headers.get("Date").get(0), ESIAccount.formatter).toEpochSecond();
+					}
 				}
-				jobsCacheEnd = Math.min(jobsCacheEnd,
-						System.currentTimeMillis()
-						+ 1000 * ZonedDateTime.parse(headers.get("Expires").get(0), ESIAccount.formatter).toEpochSecond()
-						- 1000 * ZonedDateTime.parse(headers.get("Date").get(0), ESIAccount.formatter).toEpochSecond());
+				jobsCache.clear();
+				jobsCache.addAll(ret);
 			}
-			jobsCache.clear();
-			jobsCache.addAll(ret);
 		}
 		return jobsCache;
+	}
+
+	private long bookmarkCacheEnd = 0;
+
+	private ObservableMap<String, ObservableMap<Integer, R_get_characters_character_id_bookmarks>> cacheBookmarks = FXCollections
+			.observableMap(new LinkedHashMap<>());
+
+	/**
+	 *
+	 * @return the cached list of observable bookmarks, by folder->id->bookmark.
+	 */
+	public ObservableMap<String, ObservableMap<Integer, R_get_characters_character_id_bookmarks>> getBookmarks() {
+		synchronized (cacheBookmarks) {
+			if (System.currentTimeMillis() >= bookmarkCacheEnd) {
+				// first we get all the folders.
+				HashMap<Integer, String> folders = new HashMap<>();
+				int maxPage = 1;
+				for (int page = 1; page <= maxPage; page++) {
+					Map<String, List<String>> headers = new HashMap<>();
+					for (R_get_characters_character_id_bookmarks_folders f : con.raw
+							.get_characters_character_id_bookmarks_folders(con.characterId(), page, headers)) {
+						folders.put(f.folder_id, f.name);
+					}
+					if (page == 1) {
+						String pages = headers.containsKey("x-pages") ? headers.get("x-pages").get(0) : null;
+						maxPage = pages == null ? 1 : Integer.parseInt(pages);
+						bookmarkCacheEnd = System.currentTimeMillis()
+								+ 1000 * ZonedDateTime.parse(headers.get("Expires").get(0), ESIAccount.formatter).toEpochSecond()
+								- 1000 * ZonedDateTime.parse(headers.get("Date").get(0), ESIAccount.formatter).toEpochSecond();
+					}
+				}
+				cacheBookmarks.keySet().retainAll(folders.values());
+				maxPage = 1;
+				for (int page = 1; page <= maxPage; page++) {
+					Map<String, List<String>> headers = new HashMap<>();
+					for (R_get_characters_character_id_bookmarks f : con.raw
+							.get_characters_character_id_bookmarks(con.characterId(), page, headers)) {
+						String foldName = folders.get(f.folder_id);
+						ObservableMap<Integer, R_get_characters_character_id_bookmarks> m = cacheBookmarks.get(foldName);
+						if (m == null) {
+							m = FXCollections.observableMap(new LinkedHashMap<>());
+							cacheBookmarks.put(foldName, m);
+						}
+						m.put(f.bookmark_id, f);
+					}
+					if (page == 1) {
+						String pages = headers.containsKey("x-pages") ? headers.get("x-pages").get(0) : null;
+						maxPage = pages == null ? 1 : Integer.parseInt(pages);
+						bookmarkCacheEnd = System.currentTimeMillis()
+								+ 1000 * ZonedDateTime.parse(headers.get("Expires").get(0), ESIAccount.formatter).toEpochSecond()
+								- 1000 * ZonedDateTime.parse(headers.get("Date").get(0), ESIAccount.formatter).toEpochSecond();
+					}
+				}
+			}
+		}
+		return cacheBookmarks;
 	}
 
 }
