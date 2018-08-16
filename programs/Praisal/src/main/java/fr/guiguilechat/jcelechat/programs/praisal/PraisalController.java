ca74d87dbfa6656b5b865a74ce7b7cd56b187285
guigui lechat <lechatguigui@gmail.com>
2018-08-16 06:48:56 +0200

    corrected praisal with total BO for items and 2nd bb

--- programs/Praisal/src/main/java/fr/guiguilechat/jcelechat/programs/praisal/PraisalController.java
+++ programs/Praisal/src/main/java/fr/guiguilechat/jcelechat/programs/praisal/PraisalController.java
@@ -1,6 +1,8 @@
 package fr.guiguilechat.jcelechat.programs.praisal;
 
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import fr.guiguilechat.jcelechat.esi.disconnected.modeled.ESIAccess;
 import fr.guiguilechat.jcelechat.esi.disconnected.modeled.market.RegionalMarket;
@@ -11,6 +13,7 @@ import fr.guiguilechat.jcelechat.model.sde.locations.LocationHelper;
 import fr.guiguilechat.jcelechat.model.sde.locations.Region;
 import fr.guiguilechat.tools.JFXTools;
 import fr.guiguilechat.tools.PriceCellFactory;
+import javafx.application.Platform;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.Property;
 import javafx.beans.property.ReadOnlyObjectWrapper;
@@ -19,6 +22,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.SortType;
@@ -35,33 +39,51 @@ public class PraisalController {
 	private TextArea copyfield;
 
 	@FXML
-	private TextField volumicprice;
+	private TextField bbk1volumicprice;
 
-	private DoubleProperty volumicpriceproperty;
-
-	@FXML
-	private TextField buybacktax;
-
-	private DoubleProperty buybacktaxproperty;
-
+	private DoubleProperty bbk1volumicpriceProperty;
 
 	@FXML
-	private TableView<Item> table;
+	private TextField bbk1tax;
+
+	private DoubleProperty bbk1taxProperty;
 
 	@FXML
-	private TableColumn<Item, String> itemname;
+	private TextField bbk2volumicprice;
+
+	private DoubleProperty bbk2volumicpriceProperty;
 
 	@FXML
-	private TableColumn<Item, Double> itembo;
+	private TextField bbk2tax;
+
+	private DoubleProperty bbk2taxProperty;
 
 	@FXML
-	private TableColumn<Item, Double> itemavgvol;
+	private CheckBox bbk1;
 
 	@FXML
-	private TableColumn<Item, Double> itembovol;
+	private CheckBox bbk2;
 
 	@FXML
-	private TableColumn<Item, Double> itembuyback;
+	private TableView<Entry<Item, Integer>> table;
+
+	@FXML
+	private TableColumn<Entry<Item, Integer>, String> itemname;
+
+	@FXML
+	private TableColumn<Entry<Item, Integer>, Double> itembo;
+
+	@FXML
+	private TableColumn<Entry<Item, Integer>, Double> itemavgvol;
+
+	@FXML
+	private TableColumn<Entry<Item, Integer>, Double> itembovol;
+
+	@FXML
+	private TableColumn<Entry<Item, Integer>, Double> itembuyback1;
+
+	@FXML
+	private TableColumn<Entry<Item, Integer>, Double> itembuyback2;
 
 	private Property<RegionalMarket> marketHolder = new SimpleObjectProperty<>();
 
@@ -69,36 +91,54 @@ public class PraisalController {
 	private void initialize() {
 		regionSelect.getSelectionModel().selectedItemProperty().addListener(this::changeRegion);
 
-		buybacktaxproperty = JFXTools.convertDouble(buybacktax.textProperty(), d -> d >= 0);
-		buybacktaxproperty.addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> table.sort());
+		bbk1taxProperty = JFXTools.convertDouble(bbk1tax.textProperty(), d -> d >= 0);
+		bbk1taxProperty.addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> table.sort());
 
-		volumicpriceproperty = JFXTools.convertDouble(volumicprice.textProperty(), d -> d >= 0);
-		volumicpriceproperty.addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> table.sort());
+		bbk1volumicpriceProperty = JFXTools.convertDouble(bbk1volumicprice.textProperty(), d -> d >= 0);
+		bbk1volumicpriceProperty.addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> table.sort());
+
+		bbk2taxProperty = JFXTools.convertDouble(bbk2tax.textProperty(), d -> d >= 0);
+		bbk2taxProperty.addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> table.sort());
+
+		bbk2volumicpriceProperty = JFXTools.convertDouble(bbk2volumicprice.textProperty(), d -> d >= 0);
+		bbk2volumicpriceProperty.addListener((ChangeListener<Number>) (observable, oldValue, newValue) -> table.sort());
 
 		copyfield.textProperty().addListener(this::onCopyUpdate);
+		copyfield.focusedProperty().addListener(
+				(ChangeListener<Boolean>) (observable, oldValue, newValue) -> Platform.runLater(() -> copyfield.selectAll()));
 
 		// make the columns access
-		itemname.setCellValueFactory(cell -> new ReadOnlyObjectWrapper<>(cell.getValue().name));
+		itemname.setCellValueFactory(cell -> new ReadOnlyObjectWrapper<>(cell.getValue().getKey().name));
 
-		itembo.setCellValueFactory(cell -> MarketHelpers.bo(cell.getValue().id, marketHolder).asObject());
+		itembo.setCellValueFactory(cell -> MarketHelpers.bo(cell.getValue().getKey().id, marketHolder)
+				.multiply(cell.getValue().getValue()).asObject());
 		itembo.setCellFactory(col -> new PriceCellFactory<>());
 
-		itemavgvol.setCellValueFactory(
-				cell -> MarketHelpers.monthlyAVG(cell.getValue().id, marketHolder).divide(cell.getValue().volume).asObject());
+		itemavgvol.setCellValueFactory(cell -> MarketHelpers.monthlyAVG(cell.getValue().getKey().id, marketHolder)
+				.divide(cell.getValue().getKey().volume).asObject());
 		itemavgvol.setCellFactory(col -> new PriceCellFactory<>());
 
-		itembovol.setCellValueFactory(
-				cell -> MarketHelpers.bo(cell.getValue().id, marketHolder).divide(cell.getValue().volume).asObject());
+		itembovol.setCellValueFactory(cell -> MarketHelpers.bo(cell.getValue().getKey().id, marketHolder)
+				.divide(cell.getValue().getKey().volume).asObject());
 		itembovol.setCellFactory(col -> new PriceCellFactory<>());
 
 		// buyback gain = bo *(1-tax/100) - volume*volumicprice
-		itembuyback.setCellValueFactory(cell -> MarketHelpers.bo(cell.getValue().id, marketHolder)
-				.multiply(buybacktaxproperty.divide(100).negate().add(1.0))
-				.subtract(volumicpriceproperty.multiply(cell.getValue().volume)).asObject());
-		itembuyback.setCellFactory(col -> new PriceCellFactory<>());
+		itembuyback1.setCellValueFactory(cell -> MarketHelpers.bo(cell.getValue().getKey().id, marketHolder)
+				.multiply(bbk1taxProperty.divide(100).negate().add(1.0))
+				.subtract(bbk1volumicpriceProperty.multiply(cell.getValue().getKey().volume))
+				.multiply(cell.getValue().getValue()).asObject());
+		itembuyback1.setCellFactory(col -> new PriceCellFactory<>());
+		itembuyback1.visibleProperty().bind(bbk1.selectedProperty());
+		itembuyback1.setSortType(SortType.DESCENDING);
 
-		itembuyback.setSortType(SortType.DESCENDING);
-		table.getSortOrder().add(itembuyback);
+		itembuyback2.setCellValueFactory(cell -> MarketHelpers.bo(cell.getValue().getKey().id, marketHolder)
+				.multiply(bbk2taxProperty.divide(100).negate().add(1.0))
+				.subtract(bbk2volumicpriceProperty.multiply(cell.getValue().getKey().volume))
+				.multiply(cell.getValue().getValue()).asObject());
+		itembuyback2.setCellFactory(col -> new PriceCellFactory<>());
+		itembuyback2.visibleProperty().bind(bbk2.selectedProperty());
+
+		table.getSortOrder().add(itembo);
 		table.setItems(FXCollections.observableArrayList());
 
 		new Thread(this::load).start();
@@ -119,13 +159,22 @@ public class PraisalController {
 
 	protected void onCopyUpdate(Object o, String previous, String now) {
 		table.getItems().clear();
-		parse(now).forEach(table.getItems()::add);
+		parse(now).entrySet().forEach(table.getItems()::add);
 		table.sort();
 	}
 
-	public static Stream<Item> parse(String list) {
-		System.err.println("parsint items : \n" + list);
-		return Stream.of(list.split("\n")).map(arr -> MetaInf.getItem(arr.split("\t")[0])).filter(i -> i != null);
+	public static Map<Item, Integer> parse(String list) {
+		HashMap<Item, Integer> item2qtty = new HashMap<>();
+		for (String line : list.split("\n")) {
+			String[] arr = line.split("\t");
+			Item it = MetaInf.getItem(arr[0]);
+			if (it != null && arr[1].length() > 0) {
+				item2qtty.put(it, Integer.parseInt(arr[1].replaceAll("[^0-9]", "")) + item2qtty.getOrDefault(it, 0));
+			}
+		}
+		return item2qtty;
+		// return Stream.of(list.split("\n")).map(arr ->
+		// MetaInf.getItem(arr.split("\t")[0])).filter(i -> i != null);
 	}
 
 }
