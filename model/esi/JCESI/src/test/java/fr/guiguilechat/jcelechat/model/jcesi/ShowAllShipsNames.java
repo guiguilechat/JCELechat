192cb5dc844c9c106c4f9598322ae340718dffaa
guigui lechat <lechatguigui@gmail.com>
2018-09-02 10:46:40 +0200

    minor change in main

--- model/esi/JCESI/src/test/java/fr/guiguilechat/jcelechat/model/jcesi/ShowAllShipsNames.java
+++ model/esi/JCESI/src/test/java/fr/guiguilechat/jcelechat/model/jcesi/ShowAllShipsNames.java
@@ -1,5 +1,7 @@
 package fr.guiguilechat.jcelechat.model.jcesi;
 
+import java.util.stream.Stream;
+
 import fr.guiguilechat.jcelechat.jcesi.disconnected.CacheStatic;
 import fr.guiguilechat.jcelechat.jcesi.disconnected.ESIStatic;
 
@@ -8,10 +10,17 @@ public class ShowAllShipsNames {
 	public static void main(String[] args) {
 		int parrallelism = Runtime.getRuntime().availableProcessors() * 50;
 		System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "" + parrallelism);
-		CacheStatic cache = ESIStatic.INSTANCE.cache;
 		long time = System.currentTimeMillis();
-		cache.insurance.prices().copy().parallelStream().map(price -> cache.universe.types(price.type_id).get())
-				.forEachOrdered(t -> System.err.println(t.name + "\t" + t.type_id));
+		boolean cached = false;
+		if (cached) {
+			CacheStatic cache = ESIStatic.INSTANCE.cache;
+			cache.insurance.prices().copy().parallelStream().map(price -> cache.universe.types(price.type_id).get())
+			.forEachOrdered(t -> System.err.println(t.name + "\t" + t.type_id));
+		} else {
+			ESIStatic esi = ESIStatic.INSTANCE;
+			Stream.of(esi.get_insurance_prices(null)).parallel().map(price -> esi.get_universe_types(price.type_id, null))
+			.forEachOrdered(t -> System.err.println(t.name + "\t" + t.type_id));
+		}
 		System.err.println("total fetch " + 0.001 * (System.currentTimeMillis() - time) + "s");
 	}
 
