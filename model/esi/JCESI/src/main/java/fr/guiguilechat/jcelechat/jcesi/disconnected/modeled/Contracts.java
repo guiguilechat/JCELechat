3317ac3002f98f2b8ae8d5aba18f8486c08ce72a
guigui lechat <lechatguigui@gmail.com>
2018-10-18 11:17:33 +0200

    added bids to contract, and parallel fetch

--- model/esi/JCESI/src/main/java/fr/guiguilechat/jcelechat/jcesi/disconnected/modeled/Contracts.java
+++ model/esi/JCESI/src/main/java/fr/guiguilechat/jcelechat/jcesi/disconnected/modeled/Contracts.java
@@ -1,10 +1,15 @@
 package fr.guiguilechat.jcelechat.jcesi.disconnected.modeled;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 import fr.guiguilechat.jcelechat.jcesi.ConnectedImpl;
 import fr.guiguilechat.jcelechat.jcesi.disconnected.ESIStatic;
+import fr.guiguilechat.jcelechat.model.jcesi.compiled.responses.R_get_contracts_public_bids_contract_id;
 import fr.guiguilechat.jcelechat.model.jcesi.compiled.responses.R_get_contracts_public_items_contract_id;
 import fr.guiguilechat.jcelechat.model.jcesi.compiled.responses.R_get_contracts_public_region_id;
 import fr.guiguilechat.jcelechat.model.jcesi.impl.ObsListHolderImpl;
@@ -21,8 +26,9 @@ public class Contracts {
 	}
 
 	public static class ContractDesc {
-		R_get_contracts_public_region_id details;
-		List<R_get_contracts_public_items_contract_id> items;
+		R_get_contracts_public_region_id details = null;
+		List<R_get_contracts_public_items_contract_id> items = null;
+		List<R_get_contracts_public_bids_contract_id> bids = null;
 	}
 
 	private HashMap<Integer, ObsListHolder<ContractDesc>> caches = new HashMap<>();
@@ -46,14 +52,20 @@ public class Contracts {
 								}
 							}
 							if (c.wasAdded()) {
+								ArrayList<ContractFetcher> fetchers = new ArrayList<>();
 								for (R_get_contracts_public_region_id it : c.getAddedSubList()) {
-									ContractDesc added = new ContractDesc();
-									added.details = it;
-									added.items = ConnectedImpl
-											.loadPages((p, h) -> esiConnection.get_contracts_public_items(it.contract_id, p, h),
-													null);
-									underlying.add(added);
+									ContractFetcher fetcher = new ContractFetcher(it, underlying);
+									new Thread(fetcher).start();
+									fetchers.add(fetcher);
 								}
+								// once all fetchers have received data, the returned list is
+								// notified the first pass is done.
+								new Thread(() -> {
+									for (ContractFetcher cf : fetchers) {
+										cf.waitData();
+									}
+									CHolder.dataReceived();
+								}).start();
 							}
 						}
 					});
@@ -62,6 +74,49 @@ public class Contracts {
 			}
 		}
 		return ret;
+	}
+
+	/**
+	 * fetch the data for a contract, then add the whole details to an underlying
+	 * collection.
+	 *
+	 */
+	private class ContractFetcher implements Runnable {
+
+		R_get_contracts_public_region_id contract;
+
+		Collection<ContractDesc> storage;
+
+		public ContractFetcher(R_get_contracts_public_region_id contract, Collection<ContractDesc> storage) {
+			this.contract = contract;
+			this.storage = storage;
+		}
+
+		CountDownLatch waitData = new CountDownLatch(1);
+
+		@Override
+		public void run() {
+			ContractDesc added = new ContractDesc();
+			added.details = contract;
+			added.items = ConnectedImpl
+					.loadPages((p, h) -> esiConnection.get_contracts_public_items(contract.contract_id, p, h), null);
+			added.bids = "auction".equals(contract.type)
+					? ConnectedImpl.loadPages((p, h) -> esiConnection.get_contracts_public_bids(contract.contract_id, p, h), null)
+					: Collections.emptyList();
+			synchronized (storage) {
+				storage.add(added);
+			}
+			waitData.countDown();
+		}
+
+		public void waitData() {
+			try {
+				waitData.await();
+			} catch (InterruptedException e) {
+				throw new UnsupportedOperationException("catch this", e);
+			}
+		}
+
 	}
 
 }
