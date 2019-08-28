30d4e3a44612c163a63839648f5147b79bd3ada1
guigui lechat <lechatguigui@gmail.com>
2019-08-28 03:53:29 +0200

    .

---
+++ model/sde/SDE-Industry/src/test/java/fr/guiguilechat/jcelechat/model/sde/industry/ShowBpsByInvetionSkill.java
@@ -0,0 +1,50 @@
+package fr.guiguilechat.jcelechat.model.sde.industry;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import fr.guiguilechat.jcelechat.model.sde.industry.Blueprint.Activity;
+
+public class ShowBpsByInvetionSkill {
+
+	public static void main(String[] args) {
+		HashMap<String, Map<Integer, List<String>>> skillName2level2bps = new HashMap<>();
+		for (Blueprint blueprint : Blueprint.load().values()) {
+			String bpName = blueprint.name;
+			if (blueprint.invention != null) {
+				Activity act = blueprint.invention;
+				for (Entry<String, Integer> e : act.skills.entrySet()) {
+					String skillName = e.getKey();
+					Integer skillLvl = e.getValue();
+					Map<Integer, List<String>> level2bps = skillName2level2bps.get(skillName);
+					if (level2bps == null) {
+						level2bps = new HashMap<>();
+						skillName2level2bps.put(skillName, level2bps);
+					}
+					List<String> bps = level2bps.get(skillLvl);
+					if (bps == null) {
+						bps = new ArrayList<>();
+						level2bps.put(skillLvl, bps);
+					}
+					bps.add(bpName);
+				}
+			}
+		}
+		for (Entry<String, Map<Integer, List<String>>> e : skillName2level2bps.entrySet()) {
+			System.err.println(e.getKey());
+			for (int lvl = 0; lvl < 6; lvl++) {
+				List<String> bps = e.getValue().get(lvl);
+				if (bps != null) {
+					System.err.println("\t" + lvl);
+					for (String s : bps) {
+						System.err.println("\t\t" + s);
+					}
+				}
+			}
+		}
+	}
+
+}
