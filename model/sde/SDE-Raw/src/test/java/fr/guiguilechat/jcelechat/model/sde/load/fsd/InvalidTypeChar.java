8566649b21806807fafd607ebe8472f917fed086
guigui lechat <lechatguigui@gmail.com>
2020-02-27 10:19:02 +0100

    put invalid sde chars in txt file.

--- model/sde/SDE-Raw/src/test/java/fr/guiguilechat/jcelechat/model/sde/load/fsd/InvalidTypeChar.java
+++ model/sde/SDE-Raw/src/test/java/fr/guiguilechat/jcelechat/model/sde/load/fsd/InvalidTypeChar.java
@@ -1,12 +1,17 @@
 package fr.guiguilechat.jcelechat.model.sde.load.fsd;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Map.Entry;
 import java.util.Set;
 
 public class InvalidTypeChar {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter("target/invalidChars.txt"));
+		int nb = 0;
 		for (Entry<Integer, EtypeIDs> e : EtypeIDs.load().entrySet()) {
 			var t = e.getValue();
 			Set<String> invalidNameLanguages = new HashSet<>();
@@ -22,10 +27,16 @@ public class InvalidTypeChar {
 				}
 			}
 			if (!invalidNameLanguages.isEmpty() || !invalidDescLanguages.isEmpty()) {
-				System.out.println(e.getKey() + (invalidNameLanguages.isEmpty() ? "" : " names=" + invalidNameLanguages)
+				nb++;
+				bw.append(e.getKey() + (invalidNameLanguages.isEmpty() ? ""
+						: " names="
+						+ invalidNameLanguages)
 						+ (invalidDescLanguages.isEmpty() ? "" : " descriptions=" + invalidDescLanguages));
+				bw.newLine();
 			}
 		}
+		bw.close();
+		System.out.println("done with "+nb+" error types");
 	}
 
 }
