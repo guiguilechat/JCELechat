f754ba02ef12a3bf5a42f65a64496ba91bce42e9
guigui lechat <lechatguigui@gmail.com>
2020-03-02 19:33:08 +0100

    added drone entities attack limit listing

--- model/esi/JCESI/src/test/java/fr/guiguilechat/jcelechat/jcesi/disconnected/ShowCanAttackDrones.java
+++ model/esi/JCESI/src/test/java/fr/guiguilechat/jcelechat/jcesi/disconnected/ShowCanAttackDrones.java
@@ -13,55 +13,47 @@ import fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled.responses.get_dog
 import fr.lelouet.collectionholders.interfaces.ObsObjHolder;
 
 /**
- * show the corresponding tracking of frigate rats at varying distances.<br />
- * frigate rats are "entity" cat that have a sig <80m (so destroyers and
- * frigates), a non-null speed, and a non-null orbit range.
+ *
+ * List entities that can attack light drones, and have a signature above the
+ * minimum one.
  *
  */
-public class ShowFrigateTracking {
-
+public class ShowCanAttackDrones {
 
 	public static void main(String[] args) {
-		boolean showdetails = false;
 		R_get_universe_categories_category_id catEntities = ESIAccess.INSTANCE.universe.cache.categories(11).get();
 		List<ObsObjHolder<R_get_universe_groups_group_id>> groupholders = IntStream.of(catEntities.groups)
 				.mapToObj(gid -> ESIAccess.INSTANCE.universe.cache.groups(gid)).collect(Collectors.toList());
 		List<R_get_universe_groups_group_id> groups = groupholders.stream().map(ObsObjHolder::get)
 				.collect(Collectors.toList());
-		List<ObsObjHolder<R_get_universe_types_type_id>> typeHolders = groups
-				.stream()
+		List<ObsObjHolder<R_get_universe_types_type_id>> typeHolders = groups.stream()
 				.flatMap(g -> IntStream.of(g.types).mapToObj(tid -> ESIAccess.INSTANCE.universe.cache.types(tid)))
 				.collect(Collectors.toList());
 		List<R_get_universe_types_type_id> types = typeHolders.stream().map(ObsObjHolder::get).collect(Collectors.toList());
 		Collections.sort(types, (t1, t2) -> t1 == null || t2 == null ? 0 : t1.name.compareTo(t2.name));
-		System.out.println("name\ttracking20km\ttracking50km\ttracking80km" + (showdetails ? "\tspeed\tsig\torbit" : ""));
+		System.out.println("name(id)\tsignature\tgroup\tdronesig");
 		for (R_get_universe_types_type_id t : types) {
-			if (t == null || t.radius > 80 || t.dogma_attributes == null) {
+			if (t.dogma_attributes == null) {
 				continue;
 			}
+			float maxdronesize = -1;
 			float speed = 0;
-			float orbit = 0;
+			float signature = -1;
 			for (get_dogma_dynamic_items_type_id_item_id_dogma_attributes att : t.dogma_attributes) {
+				if (att.attribute_id == 1855) {
+					maxdronesize = att.value;
+				}
 				if (att.attribute_id == 51) {
 					speed = att.value;
 				}
-				if (att.attribute_id == 416) {
-					orbit = att.value;
+				if (att.attribute_id == 552) {
+					signature = att.value;
 				}
 			}
-			if (speed <= 0) {
-				// System.err.println("skip " + t.name + " for small speed");
-				continue;
+			if (speed > 0 && maxdronesize <= 25 && maxdronesize >= 0 && signature >= 70) {
+				var group = ESIAccess.INSTANCE.universe.cache.groups(t.group_id).get();
+				System.out.println(t.name + "(" + t.type_id + ")" + "\t" + signature + "\t" + group.name + "\t" + maxdronesize);
 			}
-			if (orbit <= 0) {
-				// System.err.println("skip " + t.name + " for small orbit");
-				continue;
-			}
-			float t20 = speed * orbit / 20000 / 20000 * 40000 / t.radius;
-			float t50 = speed * orbit / 50000 / 50000 * 40000 / t.radius;
-			float t80 = speed * orbit / 80000 / 80000 * 40000 / t.radius;
-			System.out.println(t.name + "\t" + t20 + "\t" + t50 + "\t" + t80
-					+ (showdetails ? "\t" + speed + "\t" + t.radius + "\t" + orbit : ""));
 		}
 	}
 
