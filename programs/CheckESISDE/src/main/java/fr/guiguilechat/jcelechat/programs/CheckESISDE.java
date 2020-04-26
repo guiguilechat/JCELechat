a7f62ca6e75fc772da1baf8860f8f886ff534732
guigui lechat <lechatguigui@gmail.com>
2020-04-26 18:37:47 +0200

    added a program to test the differences between ESI and SDE.

---
+++ programs/CheckESISDE/src/main/java/fr/guiguilechat/jcelechat/programs/CheckESISDE.java
@@ -0,0 +1,82 @@
+package fr.guiguilechat.jcelechat.programs;
+
+import java.util.Map.Entry;
+
+import fr.guiguilechat.jcelechat.jcesi.disconnected.ESIStatic;
+import fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled.disconnected.Dogma;
+import fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled.disconnected.Universe;
+import fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled.responses.R_get_dogma_attributes_attribute_id;
+import fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled.responses.R_get_universe_categories_category_id;
+import fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled.responses.R_get_universe_groups_group_id;
+import fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled.responses.R_get_universe_types_type_id;
+import fr.guiguilechat.jcelechat.model.sde.load.bsd.EdgmAttributeTypes;
+import fr.guiguilechat.jcelechat.model.sde.load.fsd.EcategoryIDs;
+import fr.guiguilechat.jcelechat.model.sde.load.fsd.EgroupIDs;
+import fr.guiguilechat.jcelechat.model.sde.load.fsd.EtypeIDs;
+import fr.lelouet.collectionholders.interfaces.collections.ObsMapHolder;
+
+public class CheckESISDE {
+
+	public static void main(String[] args) {
+
+		long start = System.currentTimeMillis();
+		// first preload everything
+		Universe universe = ESIStatic.INSTANCE.cache.universe;
+		Dogma dogma = ESIStatic.INSTANCE.cache.dogma;
+		ObsMapHolder<Integer, R_get_universe_categories_category_id> catMap = universe.categories()
+				.mapItems(catid -> universe.categories(catid)).toMap(h -> h.get().category_id, h -> h.get());
+		ObsMapHolder<Integer, R_get_universe_groups_group_id> groupMap = universe.groups()
+				.mapItems(groupID -> universe.groups(groupID)).toMap(h -> h.get().group_id, h -> h.get());
+		ObsMapHolder<Integer, R_get_universe_types_type_id> typeMap = universe.types()
+				.mapItems(typeID -> universe.types(typeID)).toMap(h -> h.get().type_id, h -> h.get());
+		ObsMapHolder<Integer, R_get_dogma_attributes_attribute_id> attMap = dogma.attributes()
+				.mapItems(typeID -> dogma.attributes(typeID)).toMap(h -> h.get().attribute_id, h -> h.get());
+		EcategoryIDs.load();
+		EgroupIDs.load();
+		EtypeIDs.load();
+		EdgmAttributeTypes.load();
+
+		long postLoad = System.currentTimeMillis();
+		System.out.println("loaded in " + (postLoad - start) + "ms");
+		int errors = 0;
+
+		for (Entry<Integer, EcategoryIDs> e : EcategoryIDs.load().entrySet()) {
+			EcategoryIDs sdeEntry = e.getValue();
+			R_get_universe_categories_category_id esiEntry = catMap.get(e.getKey());
+			if (sdeEntry.published != esiEntry.published) {
+				System.out.println("cat=" + e.getKey() + "(" + esiEntry.name + ")" + " esi=" + esiEntry.published + " sde="
+						+ sdeEntry.published);
+				errors++;
+			}
+		}
+		for (Entry<Integer, EgroupIDs> e : EgroupIDs.load().entrySet()) {
+			EgroupIDs sdeEntry = e.getValue();
+			R_get_universe_groups_group_id esiEntry = groupMap.get(e.getKey());
+			if (sdeEntry.published != esiEntry.published) {
+				System.out.println("group=" + e.getKey() + "(" + esiEntry.name + ")" + " esi=" + esiEntry.published + " sde="
+						+ sdeEntry.published);
+				errors++;
+			}
+		}
+		for (Entry<Integer, EtypeIDs> e : EtypeIDs.load().entrySet()) {
+			EtypeIDs sdeEntry = e.getValue();
+			R_get_universe_types_type_id esiEntry = typeMap.get(e.getKey());
+			if (sdeEntry.published != esiEntry.published) {
+				System.out.println("type=" + e.getKey() + "(" + esiEntry.name + ")" + " esi=" + esiEntry.published + " sde="
+						+ sdeEntry.published);
+				errors++;
+			}
+		}
+		for (EdgmAttributeTypes sdeEntry : EdgmAttributeTypes.load()) {
+			R_get_dogma_attributes_attribute_id esiEntry = attMap.get(sdeEntry.attributeID);
+			if (sdeEntry.published != esiEntry.published) {
+				System.out.println("att=" + sdeEntry.attributeID + "(" + esiEntry.name + ")" + " esi=" + esiEntry.published
+						+ " sde=" + sdeEntry.published);
+				errors++;
+			}
+		}
+		System.out.println("found " + errors + " errors in " + (System.currentTimeMillis() - postLoad) + " ms");
+
+	}
+
+}
