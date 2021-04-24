79e5d5f1e9c3dfc06062e2c90508038c139d46ce
guigui lechat <lechatguigui@gmail.com>
2021-04-24 22:55:33 +0200

    simple fetch skills

--- libs/spring/SpringBase/FetchSkills/src/main/java/fr/guiguilechat/jcelechat/libs/spring/springbase/fetchskills/SimpleApp.java
+++ libs/spring/SpringBase/FetchSkills/src/main/java/fr/guiguilechat/jcelechat/libs/spring/springbase/fetchskills/SimpleApp.java
@@ -9,5 +9,4 @@ public class SimpleApp {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleApp.class, args);
 	}
-
 }
---
+++ libs/spring/SpringBase/FetchSkills/src/main/java/fr/guiguilechat/jcelechat/libs/spring/springbase/fetchskills/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package fr.guiguilechat.jcelechat.libs.spring.springbase.fetchskills.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(final HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and()
+		.oauth2Login();
+	}
+
+	@Bean
+	public OAuth2AuthorizedClientManager authorizedClientManager(
+			ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository authorizedClientRepository) {
+
+		OAuth2AuthorizedClientProvider authorizedClientProvider = OAuth2AuthorizedClientProviderBuilder.builder()
+				.authorizationCode().refreshToken().clientCredentials().password().build();
+
+		DefaultOAuth2AuthorizedClientManager authorizedClientManager = new DefaultOAuth2AuthorizedClientManager(
+				clientRegistrationRepository, authorizedClientRepository);
+		authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
+
+		return authorizedClientManager;
+	}
+
+}
