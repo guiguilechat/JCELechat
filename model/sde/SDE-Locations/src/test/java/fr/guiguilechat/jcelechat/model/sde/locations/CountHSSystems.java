6134e2fad74bb530ee4362c461905eef86d283b9
guigui lechat <lechatguigui@gmail.com>
2021-08-15 19:32:46 +0200

    removed useless main, added other to the list of systems

--- model/sde/SDE-Locations/src/test/java/fr/guiguilechat/jcelechat/model/sde/locations/CountHSSystems.java
+++ model/sde/SDE-Locations/src/test/java/fr/guiguilechat/jcelechat/model/sde/locations/CountHSSystems.java
@@ -3,7 +3,12 @@ package fr.guiguilechat.jcelechat.model.sde.locations;
 public class CountHSSystems {
 
 	public static void main(String[] args) {
-		System.out.println(SolarSystem.load().values().stream().filter(sys -> sys.isHS()).count());
+		System.out.println(" HS : " + SolarSystem.load().values().stream().filter(sys -> sys.isHS()).count());
+		System.out.println(" LS : " + SolarSystem.load().values().stream().filter(sys -> sys.isLS()).count());
+		System.out.println(" NS : " + SolarSystem.load().values().stream().filter(sys -> sys.isNS()).count());
+		System.out.println(" WS : " + SolarSystem.load().values().stream().filter(sys -> sys.isWormhole).count());
+		System.out.println(
+				" other : " + SolarSystem.load().values().stream().filter(sys -> !sys.isKS && !sys.isWormhole).count());
 
 	}
 
