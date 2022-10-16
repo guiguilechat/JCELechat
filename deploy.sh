e56ad05704c1f0348632116d00d2917fdf03409f
guigui lechat <lechatguigui@gmail.com>
2022-10-16 12:23:01 +0200

    pull before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-#version=A.B.C
+#usage : VERSION=A.B.C ./deploy.sh
 
 if [ -z ${VERSION+x} ]; then echo "VERSION is unset"; exit 1 ; fi
+
+git pull && git add .
+
 mvn versions:set -DnewVersion=$VERSION && mvn deploy -T2C
 
-git add .
-git commit -m "v$VERSION" && git tag "$VERSION" && git push && git push origin "$VERSION" 
+git commit -m "v$VERSION"\
+ && git tag "$VERSION"\
+ && git push\
+ && git push origin "$VERSION" 
