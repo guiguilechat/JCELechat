1a93ed95537abbb8095e58fab395430b1f8068bd
guigui lechat <lechatguigui@gmail.com>
2022-11-12 12:38:38 +0100

    varify hull

--- programs/ShowAttributes/src/main/java/fr/guiguilechat/jcelechat/programs/showattributes/ShowShipsForHull.java
+++ programs/ShowAttributes/src/main/java/fr/guiguilechat/jcelechat/programs/showattributes/ShowShipsForHull.java
@@ -8,11 +8,13 @@ import fr.guiguilechat.jcelechat.model.sde.types.Ship;
 
 public class ShowShipsForHull {
 
+	protected static final float hull = 4551;
+
 	public static void main(String[] args) {
 		Map<Ship, Double> map = Ship.METACAT.load().values().stream()
-				.collect(Collectors.toMap(s -> s, s -> Math.abs(Math.log(s.hp / 2701))));
+				.collect(Collectors.toMap(s -> s, s -> Math.abs(Math.log(s.hp / hull))));
 		map.entrySet().stream().sorted(Comparator.comparing(e -> e.getValue()))
-				.forEach(s -> System.out.println(s.getKey().name + "\t" + s.getKey().hp));
+		.forEach(s -> System.out.println(s.getKey().name + "\t" + s.getKey().hp));
 	}
 
 }
