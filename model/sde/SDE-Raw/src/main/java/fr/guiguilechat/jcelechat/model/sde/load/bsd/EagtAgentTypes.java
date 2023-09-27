3a60c98e512213a3832b8a2f7b94b7d37b7cf97d
guigui lechat <lechatguigui@gmail.com>
2023-09-27 12:36:33 +0200

    removed versions specified in parent, removed unused deps, managed children version.

--- model/sde/SDE-Raw/src/main/java/fr/guiguilechat/jcelechat/model/sde/load/bsd/EagtAgentTypes.java
+++ model/sde/SDE-Raw/src/main/java/fr/guiguilechat/jcelechat/model/sde/load/bsd/EagtAgentTypes.java
@@ -21,7 +21,6 @@ public class EagtAgentTypes {
 	public static final File FILE = new File(SDECache.INSTANCE.cacheDir(), "sde/bsd/agtAgentTypes.yaml");
 	private static ArrayList<EagtAgentTypes> cache;
 
-	@SuppressWarnings("unchecked")
 	public static synchronized ArrayList<EagtAgentTypes> load() {
 		if (cache == null) {
 			SDECache.INSTANCE.donwloadSDE();
