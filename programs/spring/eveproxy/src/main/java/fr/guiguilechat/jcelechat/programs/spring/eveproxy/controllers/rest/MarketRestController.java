55f15cf6fa1f19d39a86f65fbea36a6fedb9583f
guigui lechat <lechatguigui@gmail.com>
2024-01-17 09:19:51 +0100

    update market controller to show prices by price threshold

--- programs/spring/eveproxy/src/main/java/fr/guiguilechat/jcelechat/programs/spring/eveproxy/controllers/rest/MarketRestController.java
+++ programs/spring/eveproxy/src/main/java/fr/guiguilechat/jcelechat/programs/spring/eveproxy/controllers/rest/MarketRestController.java
@@ -1,6 +1,10 @@
 package fr.guiguilechat.jcelechat.programs.spring.eveproxy.controllers.rest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,59 +49,109 @@ public class MarketRestController {
 	}
 
 	record PriceFilter(int typeId, Integer regionId, Long locationId) {
+	}
+
+	record VolumePrice(long volume, double price) {
+		static VolumePrice of(Map.Entry<Long, Double> price) {
+			return new VolumePrice(price.getKey(), price.getValue());
+		}
+	};
+
+	@SuppressWarnings("serial")
+	static class OfferStats extends ArrayList<VolumePrice> {
+
+		public static OfferStats of(VolumePrice... prices) {
+			var ret = new OfferStats();
+			if (prices != null) {
+				ret.addAll(Arrays.asList(prices));
+			}
+			return ret;
+		}
+
+		public static OfferStats of(Map<Long, Double> pricesByVolume) {
+			var ret = new OfferStats();
+			ret.addAll(pricesByVolume.entrySet().stream().map(VolumePrice::of)
+					.sorted(Comparator.comparing(VolumePrice::volume)).toList());
+			return ret;
+		}
 
 	}
 
-	record OffersStat(
-			/** best price offered for the type */
-			double best,
-			/** volume of orders with price around 1% of the best */
-			long vol1pc,
-			/** volume of orders with price around 5% of best */
-			long vol5pc,
-			/** total volume or orders available */
-			long volume) {
-	}
-
-	record TypeMarketStats(PriceFilter filter, OffersStat sell, OffersStat buy) {
+	record TypeMarketStats(PriceFilter filter, OfferStats sellorders, OfferStats buyorders) {
 
 		static TypeMarketStats of(int typeId, Integer regionId, Long locationId, List<RegionLine> bos,
 				List<RegionLine> sos) {
 			PriceFilter filter = new PriceFilter(typeId, regionId, locationId);
-			double so = Double.NaN;
-			long volSO = 0l;
-			long volSO1pct = 0l;
-			long volSO5pct = 0l;
+			double bestSo = Double.NaN;
+			double worstSo = Double.NaN;
+			long volSoTot = 0l;
+			long volSo1pct = 0l;
+			double bestSo1pct = Double.NaN;
+			long volSo5pct = 0l;
+			double bestSo5pct = Double.NaN;
+			long volSo10pct = 0l;
+			double bestSo10pct = Double.NaN;
 			if (!sos.isEmpty()) {
-				so = sos.get(0).getOrder().price;
+				bestSo = bestSo1pct = bestSo5pct = bestSo10pct = sos.get(0).getOrder().price;
+				worstSo = sos.get(sos.size() - 1).getOrder().price;
 				for (RegionLine l : sos) {
-					volSO += l.getOrder().volume_remain;
-					if (l.getOrder().price <= 1.01 * so) {
-						volSO1pct += l.getOrder().volume_remain;
+					volSoTot += l.getOrder().volume_remain;
+					if (l.getOrder().price <= 1.01 * bestSo) {
+						volSo1pct += l.getOrder().volume_remain;
+						bestSo1pct = Math.max(bestSo1pct, l.getOrder().price);
 					}
-					if (l.getOrder().price <= 1.05 * so) {
-						volSO5pct += l.getOrder().volume_remain;
+					if (l.getOrder().price <= 1.05 * bestSo) {
+						volSo5pct += l.getOrder().volume_remain;
+						bestSo5pct = Math.max(bestSo5pct, l.getOrder().price);
+					}
+					if (l.getOrder().price <= 1.1 * bestSo) {
+						volSo10pct += l.getOrder().volume_remain;
+						bestSo10pct = Math.max(bestSo10pct, l.getOrder().price);
 					}
 				}
 			}
-			OffersStat soStats = new OffersStat(so, volSO1pct, volSO5pct, volSO);
-			double bo = Double.NaN;
-			long volBO = 0l;
-			long volBO1pct = 0l;
-			long volBO5pct = 0l;
+			OfferStats soStats = OfferStats.of(Map.of(
+					1l, bestSo,
+					volSo1pct, bestSo1pct,
+					volSo5pct, bestSo5pct,
+					volSo10pct, bestSo10pct,
+					volSoTot, worstSo));
+
+			double bestBo = Double.NaN;
+			double worstBo = Double.NaN;
+			long volBo = 0l;
+			long volBo1pct = 0l;
+			double bestBo1pct = Double.NaN;
+			long volBo5pct = 0l;
+			double bestBo5pct = Double.NaN;
+			long volBo10pct = 0l;
+			double bestBo10pct = Double.NaN;
 			if (!bos.isEmpty()) {
-				bo = bos.get(bos.size() - 1).getOrder().price;
+				bestBo = bestBo1pct = bestBo5pct = bestBo10pct = bos.get(bos.size() - 1).getOrder().price;
+				worstBo = bos.get(0).getOrder().price;
 				for (RegionLine l : bos) {
-					volBO += l.getOrder().volume_remain;
-					if (l.getOrder().price >= 0.99 * bo) {
-						volBO1pct += l.getOrder().volume_remain;
+					volBo += l.getOrder().volume_remain;
+					if (l.getOrder().price >= 0.99 * bestBo) {
+						volBo1pct += l.getOrder().volume_remain;
+						bestBo1pct = Math.min(bestBo1pct, l.getOrder().price);
 					}
-					if (l.getOrder().price >= 0.95 * bo) {
-						volBO5pct += l.getOrder().volume_remain;
+					if (l.getOrder().price >= 0.95 * bestBo) {
+						volBo5pct += l.getOrder().volume_remain;
+						bestBo5pct = Math.min(bestBo5pct, l.getOrder().price);
+					}
+					if (l.getOrder().price >= 0.9 * bestBo) {
+						volBo10pct += l.getOrder().volume_remain;
+						bestBo10pct = Math.min(bestBo10pct, l.getOrder().price);
 					}
 				}
 			}
-			OffersStat boStats = new OffersStat(bo, volBO1pct, volBO5pct, volBO);
+			OfferStats boStats = OfferStats.of(Map.of(
+					1l, bestBo,
+					volBo1pct, bestBo1pct,
+					volBo5pct, bestBo5pct,
+					volBo10pct, bestBo10pct,
+					volBo, worstBo));
+
 			return new TypeMarketStats(filter, soStats, boStats);
 		}
 	}
