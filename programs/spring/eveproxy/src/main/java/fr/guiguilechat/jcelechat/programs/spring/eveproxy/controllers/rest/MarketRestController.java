9c81b63a26328132819e91e472451b5e652b3737
guigui lechat <lechatguigui@gmail.com>
2024-01-17 10:46:50 +0100

    refactoring market stats structure

--- programs/spring/eveproxy/src/main/java/fr/guiguilechat/jcelechat/programs/spring/eveproxy/controllers/rest/MarketRestController.java
+++ programs/spring/eveproxy/src/main/java/fr/guiguilechat/jcelechat/programs/spring/eveproxy/controllers/rest/MarketRestController.java
@@ -1,11 +1,9 @@
 package fr.guiguilechat.jcelechat.programs.spring.eveproxy.controllers.rest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
+import java.util.stream.DoubleStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -18,8 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import fr.guiguilechat.jcelechat.libs.spring.market.model.RegionLine;
 import fr.guiguilechat.jcelechat.libs.spring.market.services.RegionLineService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/market")
@@ -51,108 +52,66 @@ public class MarketRestController {
 	record PriceFilter(int typeId, Integer regionId, Long locationId) {
 	}
 
-	record VolumePrice(long volume, double price) {
-		static VolumePrice of(Map.Entry<Long, Double> price) {
-			return new VolumePrice(price.getKey(), price.getValue());
+	@RequiredArgsConstructor
+	static class PriceLimitData {
+
+		@JsonIgnore
+		private final transient double priceLimit;
+
+		@JsonIgnore
+		private final transient boolean requireInf;
+
+		public long volume = 0l;
+		public Double massPrice = null;
+		@JsonIgnore
+		private transient double totValue = 0.0;
+		public Double avgPrice = null;
+
+		protected boolean accept(double orderPrice, double limit) {
+			return requireInf ? orderPrice <= limit : orderPrice >= limit;
 		}
-	};
 
-	@SuppressWarnings("serial")
-	static class OfferStats extends ArrayList<VolumePrice> {
-
-		public static OfferStats of(VolumePrice... prices) {
-			var ret = new OfferStats();
-			if (prices != null) {
-				ret.addAll(Arrays.asList(prices));
+		public void accept(Iterable<RegionLine> lines) {
+			for (RegionLine l : lines) {
+				if (accept(l.getOrder().price, priceLimit)) {
+					volume += l.getOrder().volume_remain;
+					totValue += l.getOrder().volume_remain * l.getOrder().price;
+					if (massPrice == null || !accept(l.getOrder().price, massPrice)) {
+						massPrice = l.getOrder().price;
+					}
+				}
 			}
-			return ret;
+			if (volume > 0l) {
+				avgPrice = totValue / volume;
+			}
 		}
-
-		public static OfferStats of(Map<Long, Double> pricesByVolume) {
-			var ret = new OfferStats();
-			ret.addAll(pricesByVolume.entrySet().stream().map(VolumePrice::of)
-					.sorted(Comparator.comparing(VolumePrice::volume)).toList());
-			return ret;
-		}
-
 	}
 
-	record TypeMarketStats(PriceFilter filter, OfferStats sellorders, OfferStats buyorders) {
+	record TypeMarketStats(PriceFilter filter, List<PriceLimitData> sellorders, List<PriceLimitData> buyorders) {
 
 		static TypeMarketStats of(int typeId, Integer regionId, Long locationId, List<RegionLine> bos,
 				List<RegionLine> sos) {
 			PriceFilter filter = new PriceFilter(typeId, regionId, locationId);
-			double bestSo = Double.NaN;
-			double worstSo = Double.NaN;
-			long volSoTot = 0l;
-			long volSo1pct = 0l;
-			double bestSo1pct = Double.NaN;
-			long volSo5pct = 0l;
-			double bestSo5pct = Double.NaN;
-			long volSo10pct = 0l;
-			double bestSo10pct = Double.NaN;
+
+			List<PriceLimitData> sosData = Collections.emptyList();
 			if (!sos.isEmpty()) {
-				bestSo = bestSo1pct = bestSo5pct = bestSo10pct = sos.get(0).getOrder().price;
-				worstSo = sos.get(sos.size() - 1).getOrder().price;
-				for (RegionLine l : sos) {
-					volSoTot += l.getOrder().volume_remain;
-					if (l.getOrder().price <= 1.01 * bestSo) {
-						volSo1pct += l.getOrder().volume_remain;
-						bestSo1pct = Math.max(bestSo1pct, l.getOrder().price);
-					}
-					if (l.getOrder().price <= 1.05 * bestSo) {
-						volSo5pct += l.getOrder().volume_remain;
-						bestSo5pct = Math.max(bestSo5pct, l.getOrder().price);
-					}
-					if (l.getOrder().price <= 1.1 * bestSo) {
-						volSo10pct += l.getOrder().volume_remain;
-						bestSo10pct = Math.max(bestSo10pct, l.getOrder().price);
-					}
-				}
+				double bestSo = sos.get(0).getOrder().price;
+				sosData = DoubleStream.of(bestSo, bestSo * 1.01, bestSo * 1.05, bestSo * 1.1, Double.POSITIVE_INFINITY)
+						.mapToObj(limit -> new PriceLimitData(limit, true))
+						.peek(d -> d.accept(sos))
+						.toList();
 			}
-			OfferStats soStats = OfferStats.of(Map.of(
-					1l, bestSo,
-					volSo1pct, bestSo1pct,
-					volSo5pct, bestSo5pct,
-					volSo10pct, bestSo10pct,
-					volSoTot, worstSo));
 
-			double bestBo = Double.NaN;
-			double worstBo = Double.NaN;
-			long volBo = 0l;
-			long volBo1pct = 0l;
-			double bestBo1pct = Double.NaN;
-			long volBo5pct = 0l;
-			double bestBo5pct = Double.NaN;
-			long volBo10pct = 0l;
-			double bestBo10pct = Double.NaN;
+			List<PriceLimitData> bosData = Collections.emptyList();
 			if (!bos.isEmpty()) {
-				bestBo = bestBo1pct = bestBo5pct = bestBo10pct = bos.get(bos.size() - 1).getOrder().price;
-				worstBo = bos.get(0).getOrder().price;
-				for (RegionLine l : bos) {
-					volBo += l.getOrder().volume_remain;
-					if (l.getOrder().price >= 0.99 * bestBo) {
-						volBo1pct += l.getOrder().volume_remain;
-						bestBo1pct = Math.min(bestBo1pct, l.getOrder().price);
-					}
-					if (l.getOrder().price >= 0.95 * bestBo) {
-						volBo5pct += l.getOrder().volume_remain;
-						bestBo5pct = Math.min(bestBo5pct, l.getOrder().price);
-					}
-					if (l.getOrder().price >= 0.9 * bestBo) {
-						volBo10pct += l.getOrder().volume_remain;
-						bestBo10pct = Math.min(bestBo10pct, l.getOrder().price);
-					}
-				}
+				double bestBo = bos.get(bos.size() - 1).getOrder().price;
+				bosData = DoubleStream.of(bestBo, bestBo * 0.99, bestBo * 0.95, bestBo * 0.90, 0)
+						.mapToObj(limit -> new PriceLimitData(limit, false))
+						.peek(d -> d.accept(bos))
+						.toList();
 			}
-			OfferStats boStats = OfferStats.of(Map.of(
-					1l, bestBo,
-					volBo1pct, bestBo1pct,
-					volBo5pct, bestBo5pct,
-					volBo10pct, bestBo10pct,
-					volBo, worstBo));
 
-			return new TypeMarketStats(filter, soStats, boStats);
+			return new TypeMarketStats(filter, sosData, bosData);
 		}
 	}
 
