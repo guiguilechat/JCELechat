6f995b40e0161cc02f2fe7a71b74fc5315caaee7
guigui lechat <lechatguigui@gmail.com>
2024-02-01 22:29:54 +0100

    backward testng, also fixed test about tax

--- model/sde/industry/src/test/java/fr/guiguilechat/jcelechat/model/sde/industry/activities/ManufactureTest.java
+++ model/sde/industry/src/test/java/fr/guiguilechat/jcelechat/model/sde/industry/activities/ManufactureTest.java
@@ -7,9 +7,9 @@ public class ManufactureTest {
 
 	@Test
 	public void testSimpleCost() {
-		Assert.assertEquals(Manufacture.installationCost(1000000, 1, 4/100, 1.0, 0.25/100, false), 17500);
+		Assert.assertEquals(Manufacture.installationCost(1000000, 1, 4.0 / 100, 1.0, 0.25 / 100, false), 82500);
 	}
-	
-	
-	
+
+
+
 }
