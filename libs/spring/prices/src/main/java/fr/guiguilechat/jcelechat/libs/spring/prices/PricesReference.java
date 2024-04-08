d48f3aa9665a7d1a7cced5b7b7270efda58477a2
guigui lechat <lechatguigui@gmail.com>
2024-04-08 05:01:01 +0200

    using references to be imported in App classes instead of making all the links.

---
+++ libs/spring/prices/src/main/java/fr/guiguilechat/jcelechat/libs/spring/prices/PricesReference.java
@@ -0,0 +1,13 @@
+package fr.guiguilechat.jcelechat.libs.spring.prices;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@ComponentScan("fr.guiguilechat.jcelechat.libs.spring.prices")
+@EnableJpaRepositories("fr.guiguilechat.jcelechat.libs.spring.prices")
+@EntityScan("fr.guiguilechat.jcelechat.libs.spring.prices")
+public class PricesReference {
+}
