37a864a6714933011677715fc220de21c342d94d
guigui lechat <lechatguigui@gmail.com>
2024-04-08 04:47:58 +0200

    mevetic added tests

---
+++ programs/spring/mevetic/src/main/java/fr/guiguilechat/jcelechat/programs/spring/mevetic/SecurityConfig.java
@@ -0,0 +1,22 @@
+package fr.guiguilechat.jcelechat.programs.spring.mevetic;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(authorize -> authorize
+						.anyRequest().authenticated())
+				.oauth2Login(Customizer.withDefaults());
+		return http.build();
+	}
+}
