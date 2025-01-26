ee2225c4ea1f4a85b447c60cda46eda51fd4791c
guigui lechat <lechatguigui@gmail.com>
2025-01-26 21:13:07 +0100

    rename test sde fetcher

--- model/sde/raw/src/test/java/fr/guiguilechat/jcelechat/model/sde/UpdateSde.java
+++ model/sde/raw/src/test/java/fr/guiguilechat/jcelechat/model/sde/UpdateSde.java
@@ -2,7 +2,7 @@ package fr.guiguilechat.jcelechat.model.sde;
 
 import fr.guiguilechat.jcelechat.model.sde.load.SDECache;
 
-public class SdeFetchMain {
+public class UpdateSde {
 
 	public static void main(String[] args) {
 		SDECache.INSTANCE.donwloadSDE();
