b2d6e4bb65c739181031242fa001ed881eff646f
guigui lechat <lechatguigui@gmail.com>
2025-07-12 18:42:05 +0200

    rebuild ccp

--- model/esi/compiled/src/generated/java/fr/guiguilechat/jcelechat/model/jcesi/compiler/compiled/ESIMeta.java
+++ model/esi/compiled/src/generated/java/fr/guiguilechat/jcelechat/model/jcesi/compiler/compiled/ESIMeta.java
@@ -2,7 +2,7 @@ package fr.guiguilechat.jcelechat.model.jcesi.compiler.compiled;
 
 public class ESIMeta {
     /**
-     * day at which this library was compiled
+     * day at which this library was compiled. To send as date header for versioning
      */
-    public static final String COMPILED_DATE = "2025-07-11";
+    public static final String COMPILED_DATE = "2025-07-12";
 }
