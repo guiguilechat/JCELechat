182838d2a04488cfd91367c31ac40f3c99268c08
guigui lechat <lechatguigui@gmail.com>
2025-09-22 12:26:23 +0200

    added loader for agents in space

---
+++ model/sde/raw/src/main/java/fr/guiguilechat/jcelechat/model/sde/load/fsd/EagentsInSpace.java
@@ -0,0 +1,58 @@
+package fr.guiguilechat.jcelechat.model.sde.load.fsd;
+
+import java.util.LinkedHashMap;
+
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.NodeId;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+
+import fr.guiguilechat.jcelechat.model.sde.load.JacksonYamlLoader;
+import fr.guiguilechat.jcelechat.model.sde.load.SnakeYamlLHMLoader;
+
+/**
+ * an entry in the fsd/agentsInSpace.yaml
+ */
+public class EagentsInSpace {
+
+	//
+	// SDE loading
+	//
+
+	public static final String SDE_FILE = "fsd/agentsInSpace.yaml";
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EagentsInSpace>> LOADER_JACKSON = new JacksonYamlLoader<>(
+			SDE_FILE);
+
+	public static final SnakeYamlLHMLoader<Integer, EagentsInSpace> LOADER_SNAKEYAML = new SnakeYamlLHMLoader<>(
+			SDE_FILE) {
+
+		protected void preprocess(org.yaml.snakeyaml.nodes.Node node) {
+			if (node.getNodeId() == NodeId.mapping) {
+				MappingNode mn = (MappingNode) node;
+				if (mn.getValue().size() > 0) {
+					if (mn.getValue().stream().map(nt -> ((ScalarNode) nt.getKeyNode()).getValue())
+							.filter("typeID"::equals).findAny().isPresent()) {
+						node.setType(EagentsInSpace.class);
+					}
+				}
+			}
+		}
+	};
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EagentsInSpace>> LOADER = LOADER_SNAKEYAML;
+
+	//
+	// file structure
+	//
+
+	public int dungeonID,
+			solarSystemID,
+			spawnPointID,
+			typeID;
+
+	public static void main(String[] args) {
+		System.err.println("loaded : " + LOADER.load().size());
+		System.err.println("first dungeonid is : " + LOADER.load().entrySet().iterator().next().getValue().dungeonID);
+	}
+
+}
