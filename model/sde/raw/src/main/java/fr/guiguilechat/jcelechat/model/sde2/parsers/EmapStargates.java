862efba593dab3218c0a7e73f11844901b40f08e
guigui lechat <lechatguigui@gmail.com>
2025-09-23 12:46:17 +0200

    added the remaining "map" parsers

---
+++ model/sde/raw/src/main/java/fr/guiguilechat/jcelechat/model/sde2/parsers/EmapStargates.java
@@ -0,0 +1,67 @@
+package fr.guiguilechat.jcelechat.model.sde2.parsers;
+
+import java.util.LinkedHashMap;
+
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.NodeId;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+
+import fr.guiguilechat.jcelechat.model.sde2.parsers.inspace.InSpace;
+import fr.guiguilechat.jcelechat.model.sde2.yaml.JacksonYamlLoader;
+import fr.guiguilechat.jcelechat.model.sde2.yaml.SnakeYamlLHMLoader;
+
+public class EmapStargates extends InSpace {
+
+	//
+	// SDE loading
+	//
+
+	public static final String SDE_FILE = "mapStargates";
+	public static final String SDE_FILE_YAML = SDE_FILE + ".yaml";
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EmapStargates>> LOADER_JACKSON = new JacksonYamlLoader<>(
+			SDE_FILE_YAML);
+
+	public static final SnakeYamlLHMLoader<Integer, EmapStargates> LOADER_SNAKEYAML = new SnakeYamlLHMLoader<>(
+			SDE_FILE_YAML) {
+
+		protected void preprocess(org.yaml.snakeyaml.nodes.Node node) {
+			if (node.getNodeId() == NodeId.mapping) {
+				MappingNode mn = (MappingNode) node;
+				if (mn.getValue().size() > 0) {
+					if (mn.getValue().stream().map(nt -> ((ScalarNode) nt.getKeyNode()).getValue())
+							.filter("position"::equals).findAny().isPresent()) {
+						node.setType(EmapStargates.class);
+					}
+				}
+			}
+		}
+	};
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EmapStargates>> LOADER = LOADER_SNAKEYAML;
+
+	//
+	// file structure
+	//
+
+	public static class Destination {
+		public int solarSystemID;
+		public int stargateID;
+	}
+
+	public Destination destination;
+	public int solarSystemID;
+	public int typeID;
+
+	//
+
+	public static void main(String[] args) {
+		var data = LOADER.load();
+		System.err.println("loaded : " + data.size());
+		long withname = data.values().stream().filter(m -> m.name != null).count();
+		System.err.println("named=" + withname);
+		var first = data.entrySet().iterator().next().getValue();
+		System.err.println(
+				"first : solarSystemID=" + first.solarSystemID + " target=" + first.destination.stargateID);
+	}
+}
---
+++ model/sde/raw/src/main/java/fr/guiguilechat/jcelechat/model/sde2/parsers/EmapStars.java
@@ -0,0 +1,70 @@
+package fr.guiguilechat.jcelechat.model.sde2.parsers;
+
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
+
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.NodeId;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+
+import fr.guiguilechat.jcelechat.model.sde2.yaml.JacksonYamlLoader;
+import fr.guiguilechat.jcelechat.model.sde2.yaml.SnakeYamlLHMLoader;
+
+public class EmapStars {
+
+	//
+	// SDE loading
+	//
+
+	public static final String SDE_FILE = "mapStars";
+	public static final String SDE_FILE_YAML = SDE_FILE + ".yaml";
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EmapStars>> LOADER_JACKSON = new JacksonYamlLoader<>(
+			SDE_FILE_YAML);
+
+	public static final SnakeYamlLHMLoader<Integer, EmapStars> LOADER_SNAKEYAML = new SnakeYamlLHMLoader<>(
+			SDE_FILE_YAML) {
+
+		protected void preprocess(org.yaml.snakeyaml.nodes.Node node) {
+			if (node.getNodeId() == NodeId.mapping) {
+				MappingNode mn = (MappingNode) node;
+				if (mn.getValue().size() > 0) {
+					if (mn.getValue().stream().map(nt -> ((ScalarNode) nt.getKeyNode()).getValue())
+							.filter("solarSystemID"::equals).findAny().isPresent()) {
+						node.setType(EmapStars.class);
+					}
+				}
+			}
+		}
+	};
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EmapStars>> LOADER = LOADER_SNAKEYAML;
+
+	//
+	// file structure
+	//
+
+	public long radius;
+	public int solarSystemID;
+
+	public static class StarStatistics {
+		public BigDecimal age;
+		public BigDecimal life;
+		public BigDecimal luminosity;
+		public String spectralClass;
+		public BigDecimal temperature;
+
+	}
+
+	public StarStatistics statistics;
+	public int typeID;
+
+	//
+
+	public static void main(String[] args) {
+		var data = LOADER.load();
+		System.err.println("loaded : " + data.size());
+		var first = data.entrySet().iterator().next().getValue();
+		System.err.println("first : solarSystemId=" + first.solarSystemID + " typeID=" + first.typeID);
+	}
+}
