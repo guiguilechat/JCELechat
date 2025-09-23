b16a0627a11b7ec8150eab4a8e1c1d7c6ead497b
guigui lechat <lechatguigui@gmail.com>
2025-09-23 10:51:00 +0200

    added characterAttributes parser

---
+++ model/sde/raw/src/main/java/fr/guiguilechat/jcelechat/model/sde2/parsers/EcharacterAttributes.java
@@ -0,0 +1,67 @@
+package fr.guiguilechat.jcelechat.model.sde2.parsers;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import org.yaml.snakeyaml.nodes.MappingNode;
+import org.yaml.snakeyaml.nodes.NodeId;
+import org.yaml.snakeyaml.nodes.ScalarNode;
+
+import fr.guiguilechat.jcelechat.model.sde2.yaml.JacksonYamlLoader;
+import fr.guiguilechat.jcelechat.model.sde2.yaml.SnakeYamlLHMLoader;
+
+public class EcharacterAttributes {
+
+	//
+	// SDE loading
+	//
+
+	public static final String SDE_FILE = "characterAttributes";
+	public static final String SDE_FILE_YAML = SDE_FILE + ".yaml";
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EcharacterAttributes>> LOADER_JACKSON = new JacksonYamlLoader<>(
+			SDE_FILE_YAML);
+
+	public static final SnakeYamlLHMLoader<Integer, EcharacterAttributes> LOADER_SNAKEYAML = new SnakeYamlLHMLoader<>(
+			SDE_FILE_YAML) {
+
+		protected void preprocess(org.yaml.snakeyaml.nodes.Node node) {
+			if (node.getNodeId() == NodeId.mapping) {
+				MappingNode mn = (MappingNode) node;
+				if (mn.getValue().size() > 0) {
+					if (mn.getValue().stream().map(nt -> ((ScalarNode) nt.getKeyNode()).getValue())
+							.filter("name"::equals).findAny().isPresent()) {
+						node.setType(EcharacterAttributes.class);
+					}
+				}
+			}
+		}
+	};
+
+	public static final JacksonYamlLoader<LinkedHashMap<Integer, EcharacterAttributes>> LOADER = LOADER_SNAKEYAML;
+
+	//
+	// file structure
+	//
+
+	public String description;
+	public int iconID;
+	public HashMap<String, String> name = new HashMap<>();
+	public String notes;
+	public String shortDescription;
+
+	public String enName() {
+		return name == null ? null : name.get("en");
+	}
+
+	//
+
+	public static void main(String[] args) {
+		var data = LOADER.load();
+		System.err.println("loaded : " + data.size());
+		var first = data.entrySet().iterator().next().getValue();
+		System.err.println(
+				"first : name=" + first.enName() + " description=" + first.description);
+	}
+
+}
