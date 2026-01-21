b951cff8087e28e46e82f402e2bc0e616ab55d42
guigui lechat <lechatguigui@gmail.com>
2026-01-21 14:58:16 +0100

    unfucked boot 4 mishap

--- programs/spring/mevetic/src/main/java/fr/guiguilechat/jcelechat/programs/spring/mevetic/MEvEticApp.java
+++ programs/spring/mevetic/src/main/java/fr/guiguilechat/jcelechat/programs/spring/mevetic/MEvEticApp.java
@@ -2,8 +2,8 @@ package fr.guiguilechat.jcelechat.programs.spring.mevetic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
