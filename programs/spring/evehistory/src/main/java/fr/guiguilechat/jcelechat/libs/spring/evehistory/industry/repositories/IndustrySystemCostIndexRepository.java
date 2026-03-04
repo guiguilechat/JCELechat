2bb9e5a1b8728e744e17e91f2a38aeb2c628d691
guigui lechat <lechatguigui@gmail.com>
2026-03-04 15:01:00 +0100

    removed evehistory, now integrated into eveproxy

--- programs/spring/evehistory/src/main/java/fr/guiguilechat/jcelechat/libs/spring/evehistory/industry/repositories/IndustrySystemCostIndexRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.guiguilechat.jcelechat.libs.spring.evehistory.industry.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.guiguilechat.jcelechat.libs.spring.evehistory.industry.model.IndustrySystemCostIndex;
-
-public interface IndustrySystemCostIndexRepository extends JpaRepository<IndustrySystemCostIndex, Long> {
-
-}
