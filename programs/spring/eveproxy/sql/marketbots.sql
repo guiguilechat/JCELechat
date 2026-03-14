1e4113ee21289b4bc0694b3d456d692ada5c66a0
guigui lechat <lechatguigui@gmail.com>
2026-03-14 14:12:21 +0100

    rework bot sql

--- programs/spring/eveproxy/sql/marketbots.sql
+++ programs/spring/eveproxy/sql/marketbots.sql
@@ -1,31 +1,21 @@
 with params(days, mininterval) as(
-	values(8, 24*interval '1 hour')
+	values(8, 16*interval '1 hour')
 ),
-aggregated(order_id, type_id, hourlyupdates, dailyhours, spread, datestart, dateend, nbhours, spreadhours) as(
+aggregated(order_id, type_id, datestart, dateend, spread, spread_hours, trunc_spread_hours, trunc_hours, nbupdates) as(
 	select
 		line.order_id,
 		line.type_id,
-		1.0*count(*)
-			/(1.0*extract (epoch from greatest(	interval '1 hour' 
-													+ date_trunc('hour', max(line.date))
-													-date_trunc('hour', min(line.date)),
-												p.mininterval)
-					)/3600),
-		1.0*count(distinct(date_trunc('hour', line.date)))
-			/(1.0*extract (epoch from greatest(	interval '1 hour'
-													+ date_trunc('hour', max(line.date))
-													-date_trunc('hour', min(line.date)),
-												p.mininterval)
-					)/3600/24),
-		max(line.date)-min(line.date),
 		min(line.date),
 		max(line.date),
-		count(distinct(date_trunc('hour', line.date))),
+		greatest(max(line.date)-min(line.date), interval '1 hour'), --spread
+		1.0*extract (epoch from greatest(max(line.date)-min(line.date), interval '1 hour'))/3600, --spread_hours
 		(1.0*extract (epoch from greatest(	interval '1 hour'
-												+ date_trunc('hour', max(line.date))
-												-date_trunc('hour', min(line.date)),
-											p.mininterval)
-					)/3600)
+													+ date_trunc('hour', max(line.date))
+													-date_trunc('hour', min(line.date)),
+												p.mininterval)
+					)/3600), --trunc_spread_hours
+		count(distinct(date_trunc('hour', line.date))), --trunc_hours
+		count(*)
 	from
 		params p,
 		jcelechat_trade_orderupdate line
@@ -42,17 +32,19 @@ select
 	created.region_id,
 	created.location_id,
 	line.type_id,
-	created.is_buy_order,
+	created.is_buy_order bo,
 	it.name typename,
-	to_char(line.hourlyupdates, '99D9') "updates/h",
-	to_char(line.dailyhours, '99D9') "hours/d",
-	line.nbhours,
-	line.spread active,
-	line.dateend lastupdate,
-	case when created.date_min is not null then created.date_max else null end created,
-	deletion.date_max deleted,
+	to_char(1.0*line.nbupdates/line.spread_hours, '99D99') "updates/h",
+	to_char(1.0*line.trunc_hours/line.trunc_spread_hours*24, '99D99') "hours/d",
+	line.nbupdates,
+	line.trunc_hours,
+	date_trunc('second', line.spread) update_spread,
+	case when created.date_min is not null then to_char(created.date_max, 'YYYY-MM-DD HH24:MI') else null end order_creat_date,
+	to_char(line.datestart, 'YYYY-MM-DD HH24:MI') first_updat_date,
+	to_char(line.dateend, 'YYYY-MM-DD HH24:MI') last_updat_date,
+	to_char(deletion.date_max, 'YYYY-MM-DD HH24:MI') order_delet_date,
 	case when created.date_min is not null
-		then coalesce(deletion.date_max, current_timestamp) - created.date_max
+		then date_trunc('second', coalesce(deletion.date_max, current_timestamp) - created.date_max)
 		else null
 	end orderlife
 from
@@ -64,6 +56,7 @@ from
 where
 	created.duration <>365 -- remove ccp bots
 order by
-	line.dailyhours desc
-	, line.hourlyupdates desc
+--	1.0*line.nbupdates/line.spread_hours desc ,
+	1.0*line.trunc_hours/line.trunc_spread_hours*24 desc
+	, 1.0*line.nbupdates/line.spread_hours desc
 limit 1000
