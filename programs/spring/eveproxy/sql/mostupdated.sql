cd51fcd8ce3d1cc1fcbb6e05168c014631536a2b
guigui lechat <lechatguigui@gmail.com>
2026-03-24 17:32:32 +0100

    mostupdated uses daily activity for cross-period comparison

--- programs/spring/eveproxy/sql/mostupdated.sql
+++ programs/spring/eveproxy/sql/mostupdated.sql
@@ -1,5 +1,5 @@
 with params(days, regionId, mindayactivity) as (
-	values(31, 10000002, 16)
+	values(8, 10000002::int, 16)
 ),
 updates as(
 select
@@ -10,7 +10,7 @@ from
 	jcelechat_trade_orderupdate line
 	join jcelechat_trade_ordercreation creat on creat.order_id=line.order_id
 where
-	line.region_id=p.regionId
+	(p.regionId is null or line.region_id=p.regionId)
 	and line.date>= date_trunc('day', now()- p.days * interval '1 days')
 	and creat.duration <>365
 group by
@@ -24,7 +24,7 @@ from
 	params p,
 	jcelechat_trade_ordercreation line
 where
-	line.region_id=p.regionId
+	(p.regionId is null or line.region_id=p.regionId)
 	and line.date_max>= date_trunc('day', now() - p.days*interval '1 days')
 	and line.duration <>365
 group by
@@ -33,8 +33,8 @@ group by
 select
 	updates.type_id,
 	t.name,
-	coalesce(updates.nb, 0) nbupdates,
-	coalesce(creations.nb, 0) nbcreations,
+	to_char(1.0*coalesce(updates.nb, 0)/params.days, '9990D99') daily_upd,
+	to_char(1.0*coalesce(creations.nb, 0)/params.days, '9990D99') daily_creat,
 	to_char(100.0*coalesce(updates.nb, 0)/(coalesce(creations.nb, 0)+coalesce(updates.nb, 0)), '990D99') update_pct
 from
 	params,
